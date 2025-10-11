421821453b87d7a47ea9686073bba6c31eb08a5f
Radu-Nicolae <rnicolae04@gmail.com>
2025-10-11 19:45:34 +0200

    removed old

--- _old/_old_opendt/opendt_kafka/src/main/scala/Config.scala
+++
@@ -1,8 +0,0 @@
-package opendt
-
-object Config {
-  val BOOTSTRAP_SERVERS = "localhost:9092"
-  val TASKS_TOPIC = "tasks"
-  val FRAGMENTS_TOPIC = "fragments"
-  val SIM_TIME_RATIO = 1.0 // Real-time simulation
-}
