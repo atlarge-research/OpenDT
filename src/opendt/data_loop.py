1adab6f2f45a53fee14048507b31e783a52552dd
Stavriana Kraniti <stavkraniti01@gmail.com>
2025-11-12 12:23:39 +0100

    save progress before pulling Klemen's changes

--- src/opendt/data_loop.py
+++ src/opendt/data_loop.py
@@ -3,7 +3,6 @@ import pandas as pd
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 import os
 
-# Load environment
 INFLUX_URL = os.getenv("INFLUX_URL", "http://influxdb:8086")
 INFLUX_TOKEN = os.getenv("INFLUX_TOKEN", "OPENDT_TOKEN")
 INFLUX_ORG = os.getenv("INFLUX_ORG", "opendt")
@@ -15,7 +14,7 @@ write_api = client.write_api(write_options=None)
 fragments = pd.read_parquet("opendt/surf-workload/fragments.parquet")
 
 def loop_forever():
-    print("Starting ingestion loop...")
+    print("Starting ingestion loop")
     while True:
         for _, row in fragments.iterrows():
             point = (
